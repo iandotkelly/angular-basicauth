870c639e2982a7e85f91dd2296d6f00aad56fd9e
Ian Kelly <iandotkelly@gmail.com>
2014-06-30 21:30:40 -0400

    Complete basic test coverage

--- test/test.js
+++ test/test.js
@@ -138,6 +138,7 @@ describe('basic auth service', function () {
 			it('should invoke a request to /api/authenticate with a header', function(done) {
 
 				server.respondWith(function(request) {
+					expect(request.url).toBe('/api/authenticate');
 					expect(request.requestHeaders.Authorization).toBe('Basic aWFuQG1lOmZyZWQ=');
 					request.respond(401, {}, '');
 					done();
@@ -165,7 +166,7 @@ describe('basic auth service', function () {
 				});
 
 				server.respond();
-				$rootScope.$apply();
+				$rootScope.$digest();
 			});
 
 			it('should emit the correct events from $rootScope', function(done) {
@@ -187,7 +188,46 @@ describe('basic auth service', function () {
 				});
 
 				server.respond();
-				$rootScope.$apply();
+				$rootScope.$digest();
+			});
+		});
+
+		describe('changing the url', function() {
+
+			var authService;
+			var authDefaults;
+			var server;
+
+			beforeEach(module('angularBasicAuth'));
+
+			beforeEach(inject(function ($injector) {
+				authService = $injector.get('authService');
+				authDefaults = $injector.get('authDefaults');
+
+				// cannot use $httpBackend as the service bypasses $http
+				// for the authenticate calls to avoid a circular reference
+				// so use the sinon fake server API
+				server = sinon.fakeServer.create();
+			}));
+
+			afterEach(function() {
+				server.restore();
+			});
+
+			it('should invoke a request to /a/different/url.aspx with a header', function(done) {
+
+				// change the URL
+				authDefaults.authenticateUrl = '/a/different/url.aspx';
+
+				server.respondWith(function(request) {
+					expect(request.url).toBe('/a/different/url.aspx');
+					expect(request.requestHeaders.Authorization).toBe('Basic aWFuQG1lOmZyZWQ=');
+					request.respond(401, {}, '');
+					done();
+				});
+
+				authService.login('ian@me', 'fred');
+				server.respond();
 			});
 		});
 
@@ -243,7 +283,7 @@ describe('basic auth service', function () {
 				});
 
 				server.respond();
-				$rootScope.$apply();
+				$rootScope.$digest();
 			});
 
 			it('should emit the correct events from $rootScope', function(done) {
@@ -265,10 +305,9 @@ describe('basic auth service', function () {
 				});
 
 				server.respond();
-				$rootScope.$apply();
+				$rootScope.$digest();
 			});
 		});
-
 
 		describe('after valid credentials', function() {
 
@@ -311,5 +350,169 @@ describe('basic auth service', function () {
 			});
 		});
 
+		describe('calling #logout()', function() {
+
+			var authService;
+			var $httpBackend;
+			var $http;
+			var $rootScope;
+			var server;
+
+			beforeEach(module('angularBasicAuth'));
+
+			beforeEach(inject(function ($injector) {
+				$httpBackend = $injector.get('$httpBackend');
+				$httpBackend.resetExpectations();
+				$http = $injector.get('$http');
+				$rootScope = $injector.get('$rootScope');
+				authService = $injector.get('authService');
+
+				// cannot use $httpBackend as the service bypasses $http
+				// for the authenticate calls to avoid a circular reference
+				// so use the sinon fake server API
+				server = sinon.fakeServer.create();
+			}));
+
+			beforeEach(function(done) {
+				// perform a login
+				server.respondWith(function(request) {
+					request.respond(200, {}, '');
+				});
+
+				authService.login('ian@me', 'fred')
+				.success(function() {
+					var interval = setInterval(function() {
+						// wait for the digest to complete before calling done();
+						if (!$rootScope.$$phase) {
+							clearInterval(interval);
+							done();
+						}
+					}, 5);
+				})
+				.error(function() {
+					throw 'Failed to login';
+				});
+
+				server.respond();
+				$rootScope.$digest();
+			});
+
+			afterEach(function() {
+				$httpBackend.verifyNoOutstandingExpectation ();
+				$httpBackend.verifyNoOutstandingRequest ();
+				$httpBackend.resetExpectations();
+				server.restore();
+			});
+
+			it('should remove the credentials', function() {
+				expect(authService.username()).toBe('ian@me');
+				expect(authService.getAuth()).toBe('Basic aWFuQG1lOmZyZWQ=');
+				authService.logout();
+				expect(authService.username()).toBeNull();
+				expect(authService.getAuth()).toBeNull();
+			});
+
+
+			it('should broadcast a logout event on $rootScope', function(done) {
+
+				$rootScope.$on('logout', function() {
+					done();
+				});
+
+				authService.logout();
+			});
+
+			it('an http request should not have an Authorization header', function() {
+
+				authService.logout();
+
+				$httpBackend.expectGET('/fred').respond(function(method, url, data, headers) {
+					expect(headers.Authorization).toBeUndefined();
+					return [200, ''];
+				});
+
+				$http.get('/fred');
+				$httpBackend.flush();
+			});
+		});
+
+
+		describe('after an authentication failure on a request', function() {
+
+			var authService;
+			var $httpBackend;
+			var $http;
+			var $rootScope;
+			var server;
+
+			beforeEach(module('angularBasicAuth'));
+
+			beforeEach(inject(function ($injector) {
+				$httpBackend = $injector.get('$httpBackend');
+				$httpBackend.resetExpectations();
+				$http = $injector.get('$http');
+				$rootScope = $injector.get('$rootScope');
+				authService = $injector.get('authService');
+
+				// cannot use $httpBackend as the service bypasses $http
+				// for the authenticate calls to avoid a circular reference
+				// so use the sinon fake server API
+				server = sinon.fakeServer.create();
+			}));
+
+			beforeEach(function(done) {
+				// perform a login
+				server.respondWith(function(request) {
+					request.respond(200, {}, '');
+				});
+
+				authService.login('ian@me', 'fred')
+				.success(function() {
+					var interval = setInterval(function() {
+						// wait for the digest to complete before calling done();
+						if (!$rootScope.$$phase) {
+							clearInterval(interval);
+							done();
+						}
+					}, 5);
+				})
+				.error(function() {
+					throw 'Failed to login';
+				});
+
+				server.respond();
+				$rootScope.$digest();
+			});
+
+			afterEach(function() {
+				$httpBackend.verifyNoOutstandingExpectation ();
+				$httpBackend.verifyNoOutstandingRequest ();
+				$httpBackend.resetExpectations();
+				server.restore();
+			});
+
+			it('it should invoke a logout event', function(done) {
+
+				$rootScope.$on('logout', function() {
+					// i have no idea why this calling done() can only happen
+					// when the difest is over
+					var interval = setInterval(function() {
+						// wait for the digest to complete before calling done();
+						if (!$rootScope.$$phase) {
+							clearInterval(interval);
+							done();
+						}
+					}, 5);
+				});
+
+				$httpBackend.expectGET('/fred').respond(function(method, url, data, headers) {
+					expect(headers.Authorization).toBe('Basic aWFuQG1lOmZyZWQ=');
+					return [401, ''];
+				});
+
+				$http.get('/fred');
+				$httpBackend.flush();
+			});
+		});
 	});
 });
