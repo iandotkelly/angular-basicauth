5740305eabb43bfc5772bfcfffa9e5ba5f81cf8a
Ian Kelly <iandotkelly@gmail.com>
2014-10-05 14:32:12 -0400

    Add coverage and coveralls support - hopefully

--- test/karma-phantom.conf.js
+++ test/karma-phantom.conf.js
@@ -33,15 +33,18 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-
+      'angular-basicauth.js': 'coverage'
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
+    reporters: ['spec', 'coverage', 'coveralls'],
 
+    coverageReporter: {
+      type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
+      dir: 'coverage/'
+    },
 
     // web server port
     port: 9876,
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,15 +33,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-
+      'angular-basicauth.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
-
+    reporters: ['spec', 'coverage'],
 
     // web server port
     port: 9876,
